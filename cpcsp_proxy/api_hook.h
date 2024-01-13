1fe570134b4a8fb75bb5e3b439952a833f844d83
Vitaly Lipatov <lav@etersoft.ru>
2024-01-13 12:37:36 +0300

    cpcsp_proxy/api_hook.h: remove DECLSPEC_HIDDEN

--- cpcsp_proxy/api_hook.h
+++ cpcsp_proxy/api_hook.h
@@ -1,2 +1,2 @@
-void *set_api_hook(LPCSTR libname, LPCSTR apiname, void *hook) DECLSPEC_HIDDEN;
-void reset_api_hook(LPCSTR libname, LPCSTR apiname, void *hook) DECLSPEC_HIDDEN;
+void *set_api_hook(LPCSTR libname, LPCSTR apiname, void *hook);
+void reset_api_hook(LPCSTR libname, LPCSTR apiname, void *hook);
